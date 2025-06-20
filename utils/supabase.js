804a7a724376e81b371ee63587b6b0067b47a66a
crystalbroderick <broderick.crystal@gmail.com>
2025-06-20 13:32:24 -0500

    db utils

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,9 +1,9 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
-import 'react-native-url-polyfill/auto';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "https://wmmtpfqzseugajsdzxrb.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtbXRwZnF6c2V1Z2Fqc2R6eHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MzcyOTYsImV4cCI6MjA1NjAxMzI5Nn0.m8eiCXrUwgcq6oLMU43_5DqQ7V-kwbPvW44SlqC8HLI";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
