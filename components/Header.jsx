209c39f35fa605491110878f134ee45c69559cbd
crystalbroderick <broderick.crystal@gmail.com>
2025-07-07 13:23:14 -0500

    show avatar in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,17 @@ import ThemedText from "@/components/ThemedText";
 import { FONTS, spacingY } from "@/constants/theme.js";
 import { AntDesign } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
+import Avatar from "./Avatar";
 import BackBtn from "./BackBtn";
-const Header = ({ leftIcon, title = " ", icon, iconColor = "#808080" }) => {
+const Header = ({
+  leftIcon,
+  title = " ",
+  icon,
+  iconColor = "#808080",
+  avatar,
+  showAvatar,
+}) => {
   return (
     <View style={styles.container}>
       {leftIcon === "logo" ? (
@@ -15,13 +23,19 @@ const Header = ({ leftIcon, title = " ", icon, iconColor = "#808080" }) => {
       ) : (
         <BackBtn />
       )}
-      {title && <ThemedText style={[FONTS.h2]}>{title}</ThemedText>}
-      <AntDesign
-        name={icon}
-        size={35}
-        color={iconColor}
-        onPress={() => router.navigate("/account")}
-      />
+      <ThemedText style={[FONTS.h2]}>{title || ""}</ThemedText>
+      {showAvatar ? (
+        <Pressable onPress={() => router.navigate("/account")}>
+          <Avatar size="40" uri={avatar} />
+        </Pressable>
+      ) : (
+        <AntDesign
+          name={icon}
+          size={35}
+          color={iconColor}
+          onPress={() => router.navigate("/account")}
+        />
+      )}
     </View>
   );
 };
