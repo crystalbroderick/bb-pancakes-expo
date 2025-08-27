7a2b3cf482120b49e4809652bf0c305c4231631d
crystalbroderick <broderick.crystal@gmail.com>
2025-08-27 16:20:39 -0500

    replaced insets

--- components/SafeScreen.jsx
+++ components/SafeScreen.jsx
@@ -1,26 +1,23 @@
 import { spacingX, spacingY } from "@/constants/theme";
 import { useTheme } from "@/context/ThemeContext";
-import { View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "react-native-web";
 const SafeScreen = ({ paddingHorizontal = false, style, bg, children }) => {
-  const insets = useSafeAreaInsets();
   const { theme } = useTheme();
   return (
-    <View
+    <SafeAreaView
       style={[
         {
-          paddingTop: insets.top,
           flex: 1,
           backgroundColor: bg ? bg : theme.background,
           paddingHorizontal: paddingHorizontal ? spacingX._20 : 0,
-          paddingVertical: insets.bottom + spacingY._20,
+          paddingVertical: spacingY._20,
         },
         style,
       ]}>
       <StatusBar barStyle="dark-content" />
       {children}
-    </View>
+    </SafeAreaView>
   );
 };
 
